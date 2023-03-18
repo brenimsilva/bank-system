3b3fc7b78f9f8c2b67ffd714da23c10e59f40eec
Breno Almeida <brenimsilva7@gmail.com>
2023-03-18 00:06:22 -0300

    [Chore]: Refactoring transactions Users now can use accounts for all TransactionTypes

--- src/Account.ts
+++ src/Account.ts
@@ -4,8 +4,28 @@ import { TransactionType } from "./TransactionType.js";
 export default class Account {
     constructor(public numeroDaConta: string, public saldo: number, public donoDaConta: string, public historico: Array<Transaction> = []) {}
 
-    makeTransaction(transactionType: TransactionType, value: number, toWho?: Account) {
-        this.historico = [...this.historico, new Transaction(this, transactionType, value, toWho)]
+    makeTransaction(transactionType: TransactionType, value: number, accountReceiver?: Account) {
+        switch(transactionType) {
+            case TransactionType.DEPOSIT:
+                this.saldo += value;
+                break;
+
+            case TransactionType.SEND:
+                if(this.saldo >= value) {
+                    this.saldo -= value;
+                    accountReceiver!.saldo += value;
+                    accountReceiver!.historico = [...accountReceiver!.historico, 
+                        new Transaction(accountReceiver!, TransactionType.RECEIVE, value, this)];
+                    break;
+                } else {
+                    throw new Error("Saldo insuficiente");
+                }
+
+            case TransactionType.WITHDRAW:
+                if(this.saldo >= value) this.saldo -= value;
+                break;
+        }
+        this.historico = [...this.historico, new Transaction(this, transactionType, value, accountReceiver)]
     }
 
     getHistorico() {
--- src/User.ts
+++ src/User.ts
@@ -1,5 +1,6 @@
 import Account from "./Account.js";
 import CPF from "./CPF.js";
+import { TransactionType } from "./TransactionType.js";
 
 export default class User {
     constructor(public cpf: CPF, public nome: string, public dinheiro: number, private contas: Account[] = []) {}
@@ -15,4 +16,30 @@ export default class User {
     getAccountByNumber(accountNumber: string) {
         return this.getAccounts().filter(account => account.numeroDaConta === accountNumber)[0];
     }
+
+    deposit(accountNumber: string, value: number) {
+        if(this.dinheiro >= value) this.getAccountByNumber(accountNumber).makeTransaction(TransactionType.DEPOSIT, value);
+        else throw new Error("Saldo insuficiente");
+    }
+
+    withdraw(accountNumber: string, value: number) {
+        const accountSelected = this.contas.filter((conta) => {
+            return conta.numeroDaConta === accountNumber;
+        })[0];
+
+        if (accountSelected) accountSelected.makeTransaction(TransactionType.WITHDRAW, value); 
+        else throw new Error("User precisa ser dono da conta");
+    }
+
+    transfer(accountNumber: string, value: number, receiver: Account) {
+        const accountSelected = this.contas.filter((conta) => {
+            return conta.numeroDaConta === accountNumber;
+        })[0];
+
+        if (accountSelected) accountSelected.makeTransaction(TransactionType.SEND, value, receiver);
+        else throw new Error("User precisa ser dono da conta");
+    }
+
+
+
 }
