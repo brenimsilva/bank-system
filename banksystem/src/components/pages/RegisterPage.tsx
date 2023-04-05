d36bbac4f2e9836680813880bc7b3d6afa443646
Breno Almeida <brenimsilva7@gmail.com>
2023-04-04 22:11:04 -0300

    [Feature]: Adicionado Pages

---
+++ banksystem/src/components/pages/RegisterPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Template1 from "../templates/Template1";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <Template1 />
+    </div>
+  );
+}
